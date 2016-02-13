2c61cba7898dc7c77449c913ef450c920185f6ba
crossjs <liwenfu@crossjs.com>
2016-02-13 21:55:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# nd-i18n
+
+[![Travis](https://img.shields.io/travis/ndfront/nd-i18n.svg?style=flat-square)](https://github.com/ndfront/nd-i18n)
+[![Coveralls](https://img.shields.io/coveralls/ndfront/nd-i18n.svg?style=flat-square)](https://github.com/ndfront/nd-i18n)
+[![NPM version](https://img.shields.io/npm/v/nd-promise.svg?style=flat-square)](https://npmjs.org/package/nd-i18n)
+
+> 国际化支持
+
+## 安装
+
+```bash
+$ npm install nd-i18n --save
+```
+
+## 使用
+
+```js
+var __ = require('nd-i18n');
+// use __
+```
---
+++ index.js
@@ -0,0 +1,42 @@
+/**
+ * @module i18n
+ * @author crossjs <liwenfu@crossjs.com>
+ */
+
+'use strict';
+
+/**
+ * 翻译入口
+ * @param  {string} text text to be translated
+ * @param  {string} ns   namespace
+ * @return {string}      translating result
+ */
+var i18n = function(text, ns) {
+  if (!text) {
+    return '';
+  }
+
+  var nsArr = (typeof ns === 'string') ? ns.split(/[\.\/]/) : [i18n.defaultNS];
+
+  var result = nsArr.concat(text)
+  .reduce(function(obj, key) {
+    if (obj && obj.hasOwnProperty(key)) {
+      return obj[key];
+    }
+
+    return null;
+  }, i18n.resources);
+
+  return result === null ? text : result;
+};
+
+// 默认 namespace
+i18n.defaultNS = 'default';
+
+// 当前加载的语言资源
+i18n.resources = null;
+
+// 当前支持的语言列表
+i18n.languages = null;
+
+module.exports = i18n;
